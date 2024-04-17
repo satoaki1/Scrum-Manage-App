4b040f423981559d72c5437ce581d051bb9879fb
Satoaki Ishihara <satoakiishihara@sd.taylors.edu.my>
2024-04-17 10:46:04 +0800

    Updated MeetingView.swift

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -16,16 +16,29 @@ struct MeetingView: View {
         VStack {
             ProgressView(value: 5, total: 15)
             HStack {
-                VStack {
-                    Text("Seconds Elapsed")
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed").font(.caption)
                     Label("300", systemImage: "hourglass.tophalf.fill")
                 }
-                VStack {
-                    Text("Seconds Remaining")
-                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining").font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            Circle().strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }.accesibilityLabel("Next Speaker")
+            }
         }
+        .padding()
     }
 }
 
