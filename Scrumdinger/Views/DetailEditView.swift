65ee87796151fdf6cd3c5d82a086041f48ad63fd
Satoaki Ishihara <satoakiishihara@sd.taylors.edu.my>
2024-04-19 12:00:26 +0800

    Completed navigation and modal presentation.

---
+++ Scrumdinger/Views/DetailEditView.swift
@@ -0,0 +1,54 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by 石原聡明 on 2024/04/19.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    @State private var scrum = DailyScrum.emptyScrum
+    @State private var newAttendeeName = ""
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $scrum.title)
+                HStack {
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
+                        Text("Length")
+                    }.accessibilityValue("\(scrum.lengthInMinutes) minutes")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes").accessibilityHidden(true)
+                }
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) {attendee in
+                    Text(attendee.name)
+                }
+                .onDelete {indices in
+                    scrum.attendees.remove(atOffsets: indices)
+                }
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(action: {
+                        withAnimation{
+                            let attendee = DailyScrum.Attendee(id: UUID(), name: newAttendeeName)
+                            scrum.attendees.append(attendee)
+                            newAttendeeName = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill").accessibilityLabel("Add attendee")
+                    }.disabled(newAttendeeName.isEmpty)
+                }
+            }
+        }
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -9,12 +9,17 @@ import SwiftUI
 
 struct DetailView: View {
     let scrum: DailyScrum
+    
+    @State private var isPresentingEditView = false
+    
     var body: some View {
         List{
             Section(header: Text("Meeting Info")) {
-                Label("Start Meeting", systemImage: "timer")
-                    .font(.headline)
-                    .foregroundColor(.accentColor)
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
                 HStack {
                     Label("Length", systemImage: "clock")
                     Spacer()
@@ -29,6 +34,24 @@ struct DetailView: View {
                         .background(scrum.theme.mainColor)
                         .cornerRadius(4)
                 }.accessibilityElement(children: .combine)
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
+        }
+        .navigationTitle(scrum.title)
+        .toolbar { Button("Edit") { isPresentingEditView = true } }
+        .sheet(isPresented: $isPresentingEditView) {
+            NavigationStack { DetailEditView().navigationTitle(scrum.title).toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel") { isPresentingEditView = false }
+                }
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Done") { isPresentingEditView = false }
+                }
+            }
             }
         }
     }
